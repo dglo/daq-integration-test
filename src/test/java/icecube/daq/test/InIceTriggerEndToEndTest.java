ce60a00474215cc233a2843147ea90f4ce53ca84
Dave Glowacki <dglo@icecube.wisc.edu>
2019-08-05 07:18:13 -0500

    Issue 8711: Remove SMT Rerun fallback code; merge in SMT "quick push" fix for IceACT

--- src/test/java/icecube/daq/test/InIceTriggerEndToEndTest.java
+++ src/test/java/icecube/daq/test/InIceTriggerEndToEndTest.java
@@ -68,8 +68,8 @@ public class InIceTriggerEndToEndTest
                     !msg.startsWith("Resetting counter ") &&
                     !msg.startsWith("No match for timegate ") &&
                     !msg.startsWith("Cannot create SNDAQ alerter") &&
-                    !msg.startsWith("Using buggy SMT algorithm") &&
-                    !msg.startsWith("Using fixed SMT algorithm"))
+                    !msg.startsWith("Using slow SMT algorithm") &&
+                    !msg.startsWith("Using quick SMT algorithm"))
                 {
                     fail("Bad log message#" + i + ": " +
                          appender.getMessage(i));
